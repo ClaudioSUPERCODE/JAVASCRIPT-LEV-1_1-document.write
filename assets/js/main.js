100a237e532f4761179c85983d8fb959c8df5f75
ClaudioSUPERCODE <123948180+ClaudioSUPERCODE@users.noreply.github.com>
2023-02-17 13:32:28 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+document.write ("<h1>Hello World</h1>")
+
+document.write ("<p>Have a nice day!</p>")
+
+let myText = "Hello again."
+console.log (myText)
+
+document.write (myText)
+
+const vorname = "John"
+
+const nachname = "Schmidt"
+
+document.write ("Name: " + vorname)
+document.write ("Nachname: " + nachname)
